683a7f8533aa177472bfc5cdcde85cc774b7bb5c
Daniel Duarte <danielduarte2004@gmail.com>
2018-05-11 20:05:53 -0400

    Fixed more bugs in Middleware

--- app/Middleware/FindService.js
+++ app/Middleware/FindService.js
@@ -2,12 +2,18 @@
 
 const Service = use('App/Models/Service')
 const NotFoundException = use('App/Exceptions/NotFoundException')
+const Logger = use('Logger')
 
 class FindService {
   async handle ({ request, params: { id } }, next) {
     // call next to advance the request
     try {
       const service = await Service.query().where({ id }).with('group').fetch()
+
+      if(!(service.rows.length > 0)) throw new NotFoundException(`Service with id - ${id} not found`)
+
+      Logger.info(service)
+      
       request.body.service = service
     } catch (error) {
       throw new NotFoundException(`Service with id - ${id} not found`)
