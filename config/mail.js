701c943ef692aa1c2b5f354b11f9e6b0feb7ba99
Daniel Duarte <danielduarte2004@gmail.com>
2018-05-15 19:53:08 -0400

    Email changed

--- config/mail.js
+++ config/mail.js
@@ -26,11 +26,11 @@ module.exports = {
     driver: 'smtp',
     pool: true,
     port: Env.get('MAILGUN_SMTP_PORT', 2525),
-    host: Env.get('SMTP_HOST', Env.get('MAILGUN_SMTP_SERVER')),
+    host: Env.get('SMTP_HOST'),
     secure: false,
     auth: {
-      user: Env.get('MAIL_USERNAME', Env.get('MAILGUN_SMTP_LOGIN')),
-      pass: Env.get('MAIL_PASSWORD', Env.get('MAILGUN_SMTP_PASSWORD'))
+      user: Env.get('MAIL_USERNAME'),
+      pass: Env.get('MAIL_PASSWORD')
     },
     maxConnections: 5,
     maxMessages: 100,
